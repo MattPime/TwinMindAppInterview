fb7763cfee72ef560cd09f153137a27fe709ecef
MattPime <mattpime12@gmail.com>
2025-05-12 21:30:37 -0700

    Update chat.js

--- back/routes/chat.js
+++ back/routes/chat.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -22,4 +22,4 @@ router.post("/", async (req, res) => {
   res.end();
 });
 
-module.exports = router;
+export default router;
