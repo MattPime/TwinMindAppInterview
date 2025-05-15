8c4f179a7a5287e27fab1c7586859bd30e593da2
MattPime <mattpime12@gmail.com>
2025-05-14 20:26:30 -0700

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,30 +3,18 @@ import Login from "./pages/Login";
 import Meeting from "./pages/Meeting.jsx";
 import Summary from "./pages/Summary.jsx";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* Redirect root to /Login */}
-        <Route path="/" element={<Navigate to="/Login" replace />} />
-
-        {/* Meeting page */}
         <Route path="/meeting" element={<Meeting />} />
-
-        {/* Summary page */}
         <Route path="/summary" element={<Summary />} />
-
-        {/* Optional 404 fallback */}
-        <Route
-          path="*"
-          element={
-            <div className="p-6 text-center text-red-500">
-              404 - Page Not Found
-            </div>
           }
         />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
