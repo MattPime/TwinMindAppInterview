6345088c0890a2c6290bed79ff5d164071ac101e
MattPime <mattpime12@gmail.com>
2025-05-15 21:41:05 -0700

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Meeting from "./pages/Meeting.jsx";
 import Summary from "./pages/Summary.jsx";
+import MeetingDetails from "./pages/MeetingDetails.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/meeting" element={<Meeting />} />
         <Route path="/summary" element={<Summary />} />
+        <Route path="/meeting/:id" element={<RequireAuth> <MeetingDetails /> </RequireAuth>} />
           }
         />
       </Routes>
