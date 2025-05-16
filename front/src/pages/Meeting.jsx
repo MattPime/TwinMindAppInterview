7ba97ca4dfad523df1e0c7b93bf2bbda2f976d97
MattPime <mattpime12@gmail.com>
2025-05-15 18:01:43 -0700

    Update Meeting.jsx

--- front/src/pages/Meeting.jsx
+++ front/src/pages/Meeting.jsx
@@ -118,10 +118,7 @@ export default function Meeting() {
 
   return (
     <div className="p-6">
-      <button
-        onClick={() => navigate("/Login")}
-        className="mb-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-      >
+      <button onClick={handleSignOut} className="signout-button">
         Sign Out
       </button>
       
