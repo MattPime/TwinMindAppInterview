920984d50e12c2eee138a602788b609981c06154
MattPime <mattpime12@gmail.com>
2025-05-16 21:50:55 -0700

    Update Meeting.jsx

--- front/src/pages/Meeting.jsx
+++ front/src/pages/Meeting.jsx
@@ -17,14 +17,6 @@ import ReactDOM from "react-dom/client";
 import App from "../App.jsx";
 import "../index.css";
 
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 export default function Meeting() {
   const [recording, setRecording] = useState(false);
   const [isSpeaking, setIsSpeaking] = useState(false);
