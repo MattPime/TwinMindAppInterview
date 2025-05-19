e4aa2ffc0afda25f913404b907c1d091a1fec6d9
MattPime <mattpime12@gmail.com>
2025-05-18 18:56:18 -0700

    Create test_calendar.js

---
+++ back/tests/test_calendar.js
@@ -0,0 +1,9 @@
+/**
+ * Placeholder for testing calendar integration
+ */
+describe("Calendar Integration", () => {
+  test("should return mock time slots", () => {
+    const availableSlots = ["10:00 AM", "11:30 AM"];
+    expect(availableSlots.length).toBeGreaterThan(0);
+  });
+});
